d5d357366ff0fdd41808495588d0f214004696fa
Jim Laney <jlaney@uw.edu>
2022-05-18 09:21:09 -0700

    django-container 1.4.1

--- docker/settings.py
+++ docker/settings.py
@@ -5,8 +5,6 @@ INSTALLED_APPS += [
     'vs_listener.apps.VSListenerConfig',
 ]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
 DOCUSIGN_CONNECT_SECRET = bytes(os.getenv('DOCUSIGN_CONNECT_SECRET', ''),
                                 encoding='utf8')
 EMAIL_DOMAINS = ['uw.edu', 'washington.edu']
